8531be47e193880b385973fe10e685a682ce9e63
mason.hows11 <mason.hows11@gmail.com>
2022-12-16 11:16:51 +0330

    api article added

---
+++ app/Http/Controllers/versionOne/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\versionOne;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    //
+
+    public function store(Request $request)
+    {
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\versionOne\UserController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+
+
+
+Route::group(['prefix'=>'versionOne'],function ($router){
+
+    $router->get('articles',[ArticleController::class,'index']);
+    $router->get('users',[UserController::class,'index']);
+    $router->get('user',[UserController::class,'user']);
+
 });
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});

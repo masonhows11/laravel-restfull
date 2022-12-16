0626549ea8317f62de9cc657b28dec32098dc105
mason.hows11 <mason.hows11@gmail.com>
2022-12-16 11:28:01 +0330

    api article added

--- app/Http/Controllers/versionOne/ArticleController.php
+++ app/Http/Controllers/versionOne/ArticleController.php
@@ -4,13 +4,31 @@ namespace App\Http\Controllers\versionOne;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller
 {
     //
+    public function index(){
+
+    }
 
     public function store(Request $request)
     {
+        $data = Validator::make((array)$request,[
+           'title' => ['min:3','max:50','string','unique:articles,title']
+        ]);
+
+        if($data->fails()){
+            return response([
+               'data'=>[
+                   'message' => $data->errors()
+               ]
+            ],500);
+        }
+        return response()->json([
+           'message' => 'done','status' => 200
+        ],200);
 
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\versionOne\UserController;
 
+use App\Http\Controllers\versionOne\ArticleController;
+use App\Http\Controllers\versionOne\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +26,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix'=>'versionOne'],function ($router){
 
     $router->get('articles',[ArticleController::class,'index']);
+    $router->get('store',[ArticleController::class,'store']);
     $router->get('users',[UserController::class,'index']);
     $router->get('user',[UserController::class,'user']);
 
